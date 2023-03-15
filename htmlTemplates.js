ba6bcab8b079a521399cc23bc7e23691e2086a35
Joerg Betz <joergbetz@web.de>
2023-03-15 20:30:30 +0100

    next20

--- htmlTemplates.js
+++ htmlTemplates.js
@@ -3,9 +3,14 @@ function renderCards(type, currentPokemon, i){
     <div class="pokedex ${type}">
             <h1 class="pokemonName">${currentPokemon['name']}</h1>
             <div class="id"># ${currentPokemon['id']}</div>
-            <div class="card">
-                <img class="hover" id="pokemonImage${i}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
-                <img id="type${i}" class="icon fire" src="icons/${type}.svg">
+            <div class="card" ">                
+                <img onclick="openCard(${i})"class="hover" id="pokemonImage${i}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
+                <div class="type-container" id="card${i}"></div>
             </div>
         </div>`;
+}
+
+function renderTypes(type){
+    return `
+    <img class="icon ${type}" src="icons/${type}.svg"></img>`;
 }
--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 let currentPokemon;
+let i = 0;
+let j = 20;
 
-async function loadPokemon(){
-   
-
+async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
     let response = await fetch(url);
     allPokemons = await response.json();
+    next20();
+}
 
-    for (let i=0; i<20; i++){
+async function next20() {
+    for (i; i < j; i++) {
         let url2 = `https://pokeapi.co/api/v2/pokemon/${allPokemons['results'][i]['name']}`;
         let response2 = await fetch(url2);
         currentPokemon = await response2.json();
         type = currentPokemon['types'][0]['type']['name'];
-        console.log ('Loaded pokemon', currentPokemon);
-        document.getElementById('container').innerHTML += 
-        renderCards(type, currentPokemon, i)
+        document.getElementById('container').innerHTML +=
+            renderCards(type, currentPokemon, i)
+        types(currentPokemon, i);
     }
-
-    
-    /* renderPokemonInfo(); */
-
+    j = i + 20;
+    if (j > 1281) {
+        j = 1281;
+        document.getElementById('next-btn').classList.add('d-none');
+    }
 }
 
-function renderPokemonInfo(){
-    type = currentPokemon['types'][0]['type']['name'];
-    console.log (type);
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-    document.getElementById('id').innerHTML = currentPokemon['id'];
-    document.getElementById('pokedex').classList.add(`${type}`);
-    document.getElementById('type').classList.add(`${type}`);
-    document.getElementById('type').src = `icons/${type}.svg`;
+async function openCard(index) {
+    let url2 = `https://pokeapi.co/api/v2/pokemon/${allPokemons['results'][index]['name']}`;
+    let response2 = await fetch(url2);
+    currentPokemon = await response2.json();
+    baseStats(currentPokemon);
+    abilities(currentPokemon);
+}
+
+function types(currentPokemon, i) {
+    for (let count = 0; count < currentPokemon['types'].length; count++) {
+        type = currentPokemon['types'][count]['type']['name'];
+        document.getElementById(`card${i}`).innerHTML +=
+            renderTypes(type);
+    }
+}
+
+function baseStats(currentPokemon) {
+    hp = currentPokemon['stats'][0]['base_stat'];
+    attack = currentPokemon['stats'][1]['base_stat'];
+    defense = currentPokemon['stats'][2]['base_stat'];
+    specialAttack = currentPokemon['stats'][3]['base_stat'];
+    specialDefense = currentPokemon['stats'][4]['base_stat'];
+    speed = currentPokemon['stats'][5]['base_stat'];
+}
+
+function abilities(currentPokemon) {
+    for (let count = 0; count < currentPokemon['abilities'].length; count++) {
+        ability = currentPokemon['abilities'][count]['ability']['name'];
+        console.log(currentPokemon['name']);
+        console.log(ability);
+    }
 }
