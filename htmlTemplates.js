65703ec6b3fccba2470eaae507cad5062d15d7ec
Joerg Betz <joergbetz@web.de>
2023-03-16 21:15:52 +0100

    finished card

--- htmlTemplates.js
+++ htmlTemplates.js
@@ -1,16 +1,65 @@
 function renderCards(type, currentPokemon, i){
     return `
     <div class="pokedex ${type}">
-            <h1 class="pokemonName">${currentPokemon['name']}</h1>
-            <div class="id"># ${currentPokemon['id']}</div>
-            <div class="card" ">                
-                <img onclick="openCard(${i})"class="hover" id="pokemonImage${i}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
-                <div class="type-container" id="card${i}"></div>
-            </div>
-        </div>`;
+        <h1 class="pokemonName">${currentPokemon['name']}</h1>
+        <div class="id"># ${currentPokemon['id']}</div>
+        <div class="card">                
+            <img onclick="openCard(${i})"class="hover" id="pokemonImage${i}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
+            <div class="type-container" id="card${i}"></div>
+        </div>
+    </div>`;
 }
 
 function renderTypes(type){
     return `
     <img class="icon ${type}" src="icons/${type}.svg"></img>`;
+}
+
+function renderBaseStats(hp, attack, defense, specialAttack, specialDefense, speed){
+    return`
+    <div class="baseStatsContainer">
+        <div class="bs">
+            <span style="width:30%">HP</span>
+            <div class="graph">
+                <div class="bg1 bg" style="width: ${hp}%">${hp}</div> 
+            </div>
+        </div>
+        <div class="bs">
+            <span style="width:30%">Attack</span>
+            <div class="graph">
+                <div class="bg2 bg" style="width: ${attack}%">${attack}</div> 
+            </div>
+        </div>
+        <div class="bs">
+            <span style="width:30%">Defense</span>
+            <div class="graph">
+                <div class="bg3 bg" style="width: ${defense}%">${defense}</div> 
+            </div>
+        </div>
+        <div class="bs">
+            <span style="width:30%">Special Attack</span>
+            <div class="graph">
+                <div class="bg4 bg" style="width: ${specialAttack}%">${specialAttack}</div> 
+            </div>
+        </div>
+        <div class="bs">
+            <span style="width:30%">Special Defense</span>
+            <div class="graph">
+                <div class="bg5 bg" style="width: ${specialDefense}%">${specialDefense}</div> 
+            </div>
+        </div>
+        <div class="bs">
+            <span style="width:30%">Speed</span>
+            <div class="graph">
+                <div class="bg6 bg" style="width: ${speed}%">${speed}</div> 
+            </div>
+        </div>
+    </div>    
+    `;
+}
+
+function renderAbilities(ability){
+    return `
+    <div id="${ability}" class="ability">${ability}</div>
+    `;
 }
--- script.js
+++ script.js
@@ -29,9 +29,58 @@ async function next20() {
 async function openCard(index) {
     let url2 = `https://pokeapi.co/api/v2/pokemon/${allPokemons['results'][index]['name']}`;
     let response2 = await fetch(url2);
-    currentPokemon = await response2.json();
+    currentPokemon = await response2.json();    
+    fillOutCard(currentPokemon, index);
+}
+
+function fillOutCard(currentPokemon, index){   
+    document.getElementById('class-type').classList.remove('bug', 'dark', 'dragon', 'electric', 'fire', 'fairy', 'fighting', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water');
+    document.getElementById('class-type').classList.add(`${currentPokemon['types'][0]['type']['name']}`)
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('id').innerHTML = `# ${currentPokemon['id']}`;
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+    document.getElementById('details').classList.remove('d-none');    
     baseStats(currentPokemon);
     abilities(currentPokemon);
+    showButtons(index);
+}
+
+function showButtons(index){
+    previousIndex = index -1;
+    nextIndex = index + 1;
+    if (previousIndex >= 0){
+        document.getElementById('left-btn').innerHTML = `<img onclick="openCard(${previousIndex})" src="icons/left-arrow.svg" style="height: 50px">`;
+    }else{
+        document.getElementById('left-btn').innerHTML = `<img src="icons/left-arrow.svg" style="height: 50px; opacity: 0">`;
+    }
+    if (nextIndex <= 1281){
+        document.getElementById('right-btn').innerHTML = `<img onclick="openCard(${nextIndex})" src="icons/right-arrow.svg" style="height: 50px">`;
+    }else{
+        document.getElementById('right-btn').innerHTML = `<img src="icons/right-arrow.svg" style="height: 50px; opacity: 0">`;
+    }   
+    /* document.getElementById('right-btn').innerHTML = `<img onclick="openCard(${nextIndex})" src="icons/right-arrow.svg" style="height: 50px">`; */
+}
+
+function baseStats(currentPokemon) {
+    hp = currentPokemon['stats'][0]['base_stat'];
+    attack = currentPokemon['stats'][1]['base_stat'];
+    defense = currentPokemon['stats'][2]['base_stat'];
+    specialAttack = currentPokemon['stats'][3]['base_stat'];
+    specialDefense = currentPokemon['stats'][4]['base_stat'];
+    speed = currentPokemon['stats'][5]['base_stat'];
+    document.getElementById('baseStats').innerHTML = 
+        renderBaseStats(hp, attack, defense, specialAttack, specialDefense, speed);
+}
+
+function abilities(currentPokemon) {
+    document.getElementById('abilities').innerHTML ='';
+    for (let count = 0; count < currentPokemon['abilities'].length; count++) {
+        ability = currentPokemon['abilities'][count]['ability']['name'];
+        document.getElementById('abilities').innerHTML += 
+            renderAbilities(ability);
+        document.getElementById(`${ability}`).classList.add(`${currentPokemon['types'][0]['type']['name']}`);
+    }
+    
 }
 
 function types(currentPokemon, i) {
@@ -42,19 +91,6 @@ function types(currentPokemon, i) {
     }
 }
 
-function baseStats(currentPokemon) {
-    hp = currentPokemon['stats'][0]['base_stat'];
-    attack = currentPokemon['stats'][1]['base_stat'];
-    defense = currentPokemon['stats'][2]['base_stat'];
-    specialAttack = currentPokemon['stats'][3]['base_stat'];
-    specialDefense = currentPokemon['stats'][4]['base_stat'];
-    speed = currentPokemon['stats'][5]['base_stat'];
-}
-
-function abilities(currentPokemon) {
-    for (let count = 0; count < currentPokemon['abilities'].length; count++) {
-        ability = currentPokemon['abilities'][count]['ability']['name'];
-        console.log(currentPokemon['name']);
-        console.log(ability);
-    }
+function closeCard(){
+    document.getElementById('details').classList.add('d-none'); 
 }
